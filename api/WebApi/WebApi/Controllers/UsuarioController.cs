bb2d5fc3d2f1e66b949d81917614c780578ea16c
Gers0nJr <gersonkenedy@hotmail.com>
2020-08-19 15:58:51 -0300

    primeiro commit

---
+++ api/WebApi/WebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("/usuarios")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public UsuarioController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("")]
+        [HttpGet]
+        public ActionResult ListarTodos()
+        {
+            return Ok(_context.Usuario);
+        }
+
+    }
+}
